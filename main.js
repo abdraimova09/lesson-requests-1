35e121882b2a481d961c5d574b27eec08e6cb7e2
abdraimova09 <abdraimova09@gmail.com>
2022-06-07 11:39:06 +0600

    added post request

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ GET - получение данных
 команда для запуска json-server
 json-server -w db.json -p 8000
 */
+/*
+CRUD - Create(POST-request) Read(GET-request) Update(PUT/PATCH - requests) Delete(DELETE-request) 
+*/
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById("inp-add");
+let btnAdd = document.getElementById("btn-add");
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener("click", function () {
+  let newTodo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+});
